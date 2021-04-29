9398959fe9782a576d9e791b0b1d7087c98fc37a
meridian74 <meridian@freemail.hu>
2021-04-29 12:33:50 +0200

    all tests are done

--- src/main/java/hu/nive/ujratervezes/jurassic/JurassicPark.java
+++ src/main/java/hu/nive/ujratervezes/jurassic/JurassicPark.java
@@ -1,15 +1,39 @@
 package hu.nive.ujratervezes.jurassic;
 
+import java.sql.*;
+import java.util.ArrayList;
+import java.util.List;
+
 public class JurassicPark {
 
-    private String dbUrl;
-    private String dbUser;
-    private String dbPassword;
+    private final String dbUrl;
+    private final String dbUser;
+    private final String dbPassword;
 
     public JurassicPark(String dbUrl, String dbUser, String dbPassword) {
         this.dbUrl = dbUrl;
         this.dbUser = dbUser;
         this.dbPassword = dbPassword;
     }
+
+    public List<String> checkOverpopulation() {
+        List<String> overPopulatedDinos = new ArrayList<>();
+        try (Connection connection = DriverManager.getConnection(dbUrl, dbUser, dbPassword)) {
+            PreparedStatement statement = connection.prepareStatement(
+                    "SELECT breed " +
+                        "FROM dinosaur " +
+                        "WHERE actual > expected " +
+                        "ORDER BY BREED ASC");
+            ResultSet rs = statement.executeQuery();
+            while (rs.next()) {
+                overPopulatedDinos.add(rs.getString("breed"));
+            }
+        } catch (SQLException exception) {
+            exception.printStackTrace();
+        }
+
+        return overPopulatedDinos;
+    }
+
 
 }
