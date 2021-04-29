24fe9865a6408688af90cb6446e09ad1cbae4f21
meridian74 <meridian@freemail.hu>
2021-04-29 12:35:33 +0200

    all tests are done

--- src/main/java/hu/nive/ujratervezes/jurassic/JurassicPark.java
+++ src/main/java/hu/nive/ujratervezes/jurassic/JurassicPark.java
@@ -3,6 +3,7 @@ package hu.nive.ujratervezes.jurassic;
 import java.sql.*;
 import java.util.ArrayList;
 import java.util.List;
+import static java.sql.DriverManager.*;
 
 public class JurassicPark {
 
@@ -18,7 +19,7 @@ public class JurassicPark {
 
     public List<String> checkOverpopulation() {
         List<String> overPopulatedDinos = new ArrayList<>();
-        try (Connection connection = DriverManager.getConnection(dbUrl, dbUser, dbPassword)) {
+        try (Connection connection = getConnection(dbUrl, dbUser, dbPassword)) {
             PreparedStatement statement = connection.prepareStatement(
                     "SELECT breed " +
                         "FROM dinosaur " +
@@ -34,6 +35,4 @@ public class JurassicPark {
 
         return overPopulatedDinos;
     }
-
-
 }
